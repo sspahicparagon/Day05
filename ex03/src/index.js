100ba2d0cc981f2bb59207e3e6c7764708faa596
demon034 <sspahic2@etf.unsa.ba>
2021-03-13 01:48:11 +0100

    Javascript tasks

---
+++ ex03/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+
+    var myString = "FirstLin\n\t\SecondLine\nThirdLine";
+    // Only change code above this line
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
